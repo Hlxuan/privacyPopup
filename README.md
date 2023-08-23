b93ef8c48c08d699804ee8d363be87e6eb2c31ae
Hlxuan <huanglangxuan@hlxuan.top>
2023-08-23 17:07:27 +0800

    feat:新增首页隐私授权弹窗

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
-> 目前正在写代码，晚点上线哦～
+# 微信小程序隐私授权弹窗
+![GitHub commit activity (branch)](https://img.shields.io/github/commit-activity/m/Hlxuan/privacyPopup) ![GitHub issues](https://img.shields.io/github/issues/Hlxuan/privacyPopup) ![GitHub Stars](https://img.shields.io/github/stars/Hlxuan/privacyPopup)
 
-# 微信小程序授权弹窗
+
 
 ## 前言
-微信团队于2023年8月14日发布公告[《关于小程序隐私保护指引设置的公告》](https://mp.weixin.qq.com/cgi-bin/announce?action=getannouncement&announce_id=11691660367cfUvX&version=&lang=zh_CN&token=)，在小程序内调用与隐私相关的接口或组件，需要先在后台完善和更新《小程序用户隐私保护指引》，完善后需要开发者在小程序弹窗授权，用户同意后方可调用相关接口或组件。
+微信团队于2023年8月14日发布公告[《关于小程序隐私保护指引设置的公告》](https://mp.weixin.qq.com/cgi-bin/announce?action=getannouncement&announce_id=11691660367cfUvX&version=&lang=zh_CN&token=)，在小程序内调用与隐私相关的接口或组件，需要先在后台完善和更新《小程序用户隐私保护指引》，完善后还需要开发者在小程序内弹窗获取用户授权，用户授权同意后方可调用相关接口或组件。
 
-我这边准备了一个隐私弹窗，供大家参考。
+我封装了一个隐私授权弹窗，大家可以参考。
+![](https://images.hlxuan.top/2023/08/ac737c2946d36e40.PNG)
+
+
+## 环境要求
+小程序基础库 2.32.3 及以上（本组件已经进行了低版本兼容处理）
 
 
 ## 说明
@@ -13,11 +19,18 @@
 
 小程序隐私协议开发指南（内含官方DEMO）：[https://developers.weixin.qq.com/miniprogram/dev/framework/user-privacy/PrivacyAuthorize.html](https://developers.weixin.qq.com/miniprogram/dev/framework/user-privacy/PrivacyAuthorize.html)
 
+**使用之前需要将项目里的`AppId`换成自己的，目前是只能使用正式号来测试。**
+
 
 ## 我的网站
 [hlxuan的树屋](https://www.hlxuan.top)
 
 [Hlxuan的开放文档](https://doc.hlxuan.top)
+
+
+## License
+[Apache License 2.0](LICENSE)
+
 
 ## 支持作者
 
@@ -29,3 +42,8 @@ If you think this document will help you, you can buy the author a cup of coffee
 | 支付宝<br>Alipay                                           | 微信<br>WeChat                                             |
 | ---------------------------------------------------------- | ---------------------------------------------------------- |
 | ![](https://res.hlxuan.top/opendoc/support-author/alipay.png) | ![](https://res.hlxuan.top/opendoc/support-author/weixin.png) |
+
+
+## 公众号
+微信搜索「**黄朗轩**」关注我的公众号，我们一起探索～
+![](https://res.hlxuan.top/opendoc/gzh-banner.png)
---
+++ component/privacyPopup/privacyPopup.js
@@ -0,0 +1,121 @@
+// component/privacyPopup/privacyPopup.js
+let privacyHandler
+let privacyResolves = new Set()
+let closeOtherPagePopUpHooks = new Set()
+
+// 低版本兼容
+if (wx.onNeedPrivacyAuthorization) {
+  wx.onNeedPrivacyAuthorization(resolve => {
+    if (typeof privacyHandler === 'function') {
+      privacyHandler(resolve)
+    }
+  })
+}
+
+Component({
+
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    showPop: false, // 显示弹窗（默认关闭）
+    miniprogramName: "i朗月", // 小程序的名称
+  },
+
+  lifetimes: {
+    attached: function () {
+      // 低版本兼容
+      if (wx.onNeedPrivacyAuthorization) {
+        privacyHandler = resolve => {
+          privacyResolves.add(resolve)
+        }
+      }
+
+      // 低版本兼容
+      if (wx.getPrivacySetting) {
+        wx.getPrivacySetting({
+          success: res => {
+            console.log("getPrivacySetting success =>", res)
+            if (res.needAuthorization) {
+              // 需要弹出隐私协议
+              this.setData({
+                showPrivacy: true,
+                privacyContractName: res.privacyContractName,
+              })
+            } else {
+              // 用户已经同意过隐私协议，所以不需要再弹出隐私协议，也能调用隐私接口
+            }
+          },
+          fail: (err) => {
+            console.error("getPrivacySetting fail =>", err)
+          }
+        })
+      }
+    },
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    // 点击遮罩层时触发
+    clickoverlay() {
+      // 防止用户点击遮罩层后弹窗关闭
+      if (this.data.showPrivacy) {
+        this.setData({
+          showPrivacy: true,
+        })
+      }
+    },
+
+    // 跳转至隐私协议页面
+    openPrivacyContract() {
+      wx.openPrivacyContract({
+        success: res => {
+          console.log('openPrivacyContract success')
+        },
+        fail: res => {
+          console.error('openPrivacyContract fail', res)
+        }
+      })
+    },
+
+    // 用户拒绝
+    handleDisagree() {
+      console.log("用户拒绝隐私授权，相关的接口无法使用")
+      this.triggerEvent("disagree")
+      // 低版本兼容
+      if (privacyHandler) {
+        // 告知平台用户已经拒绝
+        privacyHandler({
+          buttonId: 'disagree-btn',
+          event: 'disagree'
+        })
+        privacyResolves.clear()
+      }
+    },
+
+    // 用户同意
+    handleAgree() {
+      console.log("用户同意隐私授权，相关的接口可以正常使用")
+      this.triggerEvent("agree")
+      // 告知平台用户已经同意，参数传同意按钮的id。为确保用户有同意的操作，基础库在 resolve 被调用后，会去检查对应的同意按钮有没有被点击过。检查通过后，相关隐私接口会继续调用
+      privacyHandler({
+        buttonId: 'agree-btn',
+        event: 'agree'
+      })
+      privacyResolves.clear()
+      this.setData({
+        showPrivacy: false,
+      })
+    },
+
+  }
+})
