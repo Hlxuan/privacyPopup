c8bea331342695b705ec5beea28da01b2dcbad6b
Hlxuan <huanglangxuan@hlxuan.top>
2023-08-24 18:28:33 +0800

    perf:取消不必要的监听、优化代码

--- pages/phoneNumber/phoneNumber.js
+++ pages/phoneNumber/phoneNumber.js
@@ -12,15 +12,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-    // 监听隐私接口需要用户授权事件
-    wx.onNeedPrivacyAuthorization(resolve => {
-      // 需要用户同意隐私授权时
-      // 弹出开发者自定义的隐私授权弹窗
-      this.setData({
-        showPrivacy: true
-      })
-      this.resolvePrivacyAuthorization = resolve
-    })
+
   },
 
   /**
@@ -79,11 +71,7 @@ Page({
 
   // 用户点击同意按钮后
   handleAgreePrivacyAuthorization() {
-    // 用户点击同意后，开发者调用 resolve({ buttonId: 'agree-btn', event: 'agree' })  告知平台用户已经同意，参数传同意按钮的id。为确保用户有同意的操作，基础库在 resolve 被调用后，会去检查对应的同意按钮有没有被点击过。检查通过后，相关隐私接口会继续调用
-    this.resolvePrivacyAuthorization({
-      buttonId: 'agree-btn',
-      event: 'agree'
-    })
+
   },
 
   // 用户点击拒绝按钮后
@@ -92,9 +80,6 @@ Page({
       title: '没有权限使用该功能',
       icon: "none",
       duration: 2000,
-    })
-    this.resolvePrivacyAuthorization({
-      event: "disagree"
     })
   },
 
